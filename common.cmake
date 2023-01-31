4bf90783eb814b6fe06b78c9c5158494874678b7
funnbot <22226942+funnbot@users.noreply.github.com>
2023-01-30 16:36:27 -0800

    initial commit

---
+++ common.cmake
@@ -0,0 +1,34 @@
+include_guard(GLOBAL)
+
+# stricter wrapper of cmake_parse_arguments
+macro(fun_parse_arguments _fun_parse_arg_num _fun_parse_prefix _fun_parse_flags _fun_parse_one_value _fun_parse_multi_value _fun_parse_required)
+
+    cmake_parse_arguments(PARSE_ARGV ${_fun_parse_arg_num} ${_fun_parse_prefix} 
+        "${_fun_parse_flags}" "${_fun_parse_one_value}" "${_fun_parse_multi_value}")
+
+    if (DEFINED ${_fun_parse_prefix}_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "unparsed arguments in function call")
+    endif()
+
+    foreach(required IN LISTS _fun_parse_required)
+        if (NOT DEFINED ${_fun_parse_prefix}_)
+            message(FATAL_ERROR "required argument is not defined: ${required}")
+        endif()
+    endforeach()
+endmacro()
+
+# TODO: testing
+# set_if(MY_VAR IF NOT NOT TRUE THEN "set to this")
+# set_if(MY_VAR IF NOT TRUE THEN "xxx" ELSE "set to this")
+function(set_if output_variable)
+    fun_parse_arguments(1 "_arg" "" "" "IF;THEN;ELSE" "IF;THEN")
+    if (${_arg_IF})
+        set(${output_variable} ${_arg_THEN} PARENT_SCOPE)
+    else()
+        if (DEFINED _arg_ELSE)
+            set(${output_variable} ${_arg_ELSE} PARENT_SCOPE)
+        else()
+            unset(${output_variable} PARENT_SCOPE)
+        endif()
+    endif()
+endfunction()
---
+++ vcpkg.cmake
@@ -0,0 +1,88 @@
+include_guard(GLOBAL)
+
+include("${CMAKE_CURRENT_LIST_DIR}/common.cmake")
+
+# clone a local vcpkg install if user doesn't already have
+# must be called before project()
+# sets the toolchain file to vcpkg.cmake
+# recommended to add /vcpkg to .gitignore
+# set VCPKG_ROOT or ENV{VCPKG_ROOT} to use an alternate vcpkg install location
+function(fun_bootstrap_vcpkg)
+    cmake_parse_arguments(PARSE_ARGV 0 _arg "NO_SYSTEM" "VERSION_TAG" "")
+
+    if(DEFINED _arg_VERSION_TAG)
+        set(version_tag_opt "")
+    else()
+        set(version_tag_opt "")
+    endif()
+
+    # custom toolchain and first run
+    if(DEFINED CMAKE_TOOLCHAIN_FILE AND NOT DEFINED VCPKG_ROOT)
+        message(STATUS "using custom toolchain, include vcpkg to build dependencies.")
+        return()
+    endif()
+
+    if(DEFINED ENV{VCPKG_ROOT})
+        set(vcpkg_default_root "$ENV{VCPKG_ROOT}")
+    else()
+        set(vcpkg_default_root "${CMAKE_SOURCE_DIR}/vcpkg")
+    endif()
+
+    set(VCPKG_ROOT "${vcpkg_default_root}" CACHE PATH "vcpkg root directory")
+    message(STATUS "vcpkg root: ${VCPKG_ROOT}")
+
+    if(WIN32)
+        set(vcpkg_cmd "${VCPKG_ROOT}/${vcpkg_cmd}")
+        set(vcpkg_bootstrap_cmd "${VCPKG_ROOT}/bootstrap-vcpkg.bat")
+    else()
+        set(vcpkg_cmd "${VCPKG_ROOT}/${vcpkg_cmd}")
+        set(vcpkg_bootstrap_cmd "${VCPKG_ROOT}/bootstrap-vcpkg.sh")
+    endif()
+
+    if(NOT EXISTS "${vcpkg_bootstrap_cmd}")
+        find_package(Git)
+        execute_process(COMMAND "${GIT_EXECUTABLE}" clone --filter=tree:0 "https://github.com/microsoft/vcpkg.git" "${VCPKG_ROOT}")
+
+        if(NOT EXISTS "${vcpkg_bootstrap_cmd}")
+            message(FATAL_ERROR "failed to clone vcpkg")
+        endif()
+    endif()
+
+    if(NOT EXISTS "${vcpkg_cmd}")
+        execute_process(COMMAND "${vcpkg_bootstrap_cmd}" -disableMetrics
+            WORKING_DIRECTORY "${VCPKG_ROOT}")
+
+        if(NOT EXISTS "${vcpkg_cmd}")
+            message(FATAL_ERROR "failed to bootstrap vcpkg")
+        endif()
+    endif()
+
+    if(NOT DEFINED CMAKE_TOOLCHAIN_FILE)
+        set(CMAKE_TOOLCHAIN_FILE "${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "")
+    endif()
+endfunction()
+
+# guess a vcpkg triplet for this platform
+function(fun_guess_vcpkg_triplet out_triplet)
+    set(target_arch "x64")
+
+    if(WIN32)
+        set(triplet "${target_arch}-windows")
+
+        if(NOT BUILD_SHARED_LIBS)
+            set(triplet "${triplet}-static")
+        endif()
+    elseif(UNIX)
+        if(APPLE)
+            set(triplet "${target_arch}-osx")
+        else()
+            set(triplet "${target_arch}-linux")
+        endif()
+
+        if(BUILD_SHARED_LIBS)
+            set(triplet "${triplet}-dynamic")
+        endif()
+    endif()
+
+    set(${out_triplet} "${triplet}" PARENT_SCOPE)
+endfunction()
